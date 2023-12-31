3bd8558632ab3ca17b69f340c0dabca29a7331de
[spakvlad] <[vladyslav.shpakov@gmail.com]>
2023-12-31 15:49:21 +0200

    Init projects (all work fine)

---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+// core version + navigation, pagination modules:
+// import Swiper from 'swiper/swiper-bundle';
+
+
+// import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+// import 'swiper/css/bundle';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+// //   modules: [Navigation, Pagination],
+// });
+
+// const swiper = new Swiper(".swiper", {
+//     direction: "horizontal",
+//     loop: true,
+//     autoHeigh: true,
+//     freeMode: true,
+//     speed: 1000,
+//     autoplay: {
+//         delay: 3000,
+//     },
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     centerdSlaides: true,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+
+//     keyboard: true,
+//     onlyInViewport: true,
+
+//     // effect: "coverflow",
+//     // coverflowEffect: {
+//     //     depth: 200,
+//     //     modifier: 0.7,
+//     //     rotate: 50,
+//     //     stretch: 100,
+
+//     //     slideShadows: true,
+//     // }
+// });
