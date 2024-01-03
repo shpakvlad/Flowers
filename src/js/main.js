4f0e11d0baf7dcf3004ec59bf11bf7b082b0b10b
[spakvlad] <[vladyslav.shpakov@gmail.com]>
2024-01-03 19:07:29 +0200

    Fix background image

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@
  * Если мы хотим добавить модуль следует его раскомментировать
  */
 // import MousePRLX from './libs/parallaxMouse'
-// import AOS from 'aos'
+import AOS from 'aos'
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 import BaseHelpers from './helpers/BaseHelpers';
@@ -44,7 +44,26 @@ BaseHelpers.checkWebpSupport();
  *  Библиотека для анимаций
  *  документация: https://michalsnik.github.io/aos
  * */
-// AOS.init();
+AOS.init({
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
 
 /**
  * Параллакс мышей
@@ -61,4 +80,27 @@ BaseHelpers.checkWebpSupport();
 // 	shouldOpenAll: false, // true
 // 	defaultOpen: [], // [0,1]
 // 	collapsedClass: 'open',
+// });
+
+// const swiper = new Swiper('.swiper', {
+//     loop: true,
+//     // freeMode: true,
+//     speed: 1000,
+//     autoplay: {
+//         delay: 3000,
+//     },
+//     slidesPerView: 'auto',
+//     spaceBetween: 18,
+//     // slidesPerGroup: 4,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     keyboard: true,
+//     onlyInViewport: true,
 // });
